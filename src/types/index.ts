22efe0e0aa212da43d62bdb80663d5c0fb8b01a9
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-25 10:38:26 +0330

    feat: add api types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,21 @@
+export interface AuthCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterPayload {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface UpdateProfilePayload {
+  name?: string;
+  bio?: string;
+  location?: string;
+  website?: string;
+}
+
 export interface ApiError {
   message: string;
   status?: number;
