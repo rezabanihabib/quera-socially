b5c56ae5c8f72a213c00e3d355fcbda42901facf
Reza Banihabib <rezabanihabib20@gmail.com>
2026-08-26 11:23:20 +0330

    fix : add navbar to layout

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { useSession } from "@/hooks/useAuth";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,6 +23,7 @@ export default function Layout() {
   return (
     <QueryClientProvider client={queryClient}>
       <SessionBootstrap>
+        <Navbar />
         <Outlet />
         <Toaster
           position="bottom-center"
